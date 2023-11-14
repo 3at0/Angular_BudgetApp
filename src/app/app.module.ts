cc0d0896c95659ed635f253c911a428dd62df936
Bato Prosic <mail@batoprosic.com>
2023-11-14 13:38:08 +0100

    Add Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataModule } from './data/data.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { DataModule } from './data/data.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DataModule
+    DataModule,
+    BrowserAnimationsModule
 
   ],
   providers: [],
